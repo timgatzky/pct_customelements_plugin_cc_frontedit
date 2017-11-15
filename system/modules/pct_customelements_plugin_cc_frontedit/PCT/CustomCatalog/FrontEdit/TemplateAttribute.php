90b95c971ba88bf3fcf8d050806666c94911e1a0
Tim Gatzky <info@tim-gatzky.de>
2017-11-15 08:34:37 +0100

    #43 process the associated fields array

--- system/modules/pct_customelements_plugin_cc_frontedit/PCT/CustomCatalog/FrontEdit/TemplateAttribute.php
+++ system/modules/pct_customelements_plugin_cc_frontedit/PCT/CustomCatalog/FrontEdit/TemplateAttribute.php
@@ -38,6 +38,7 @@ class TemplateAttribute extends \PCT\CustomElements\Core\TemplateAttribute
 			return $arrEntries;
 		}
 		$arrReturn = array();
+		$arrProcessed = array();
 		foreach($arrEntries as $i => $objRowTemplate)
 		{
 			if(count($objRowTemplate->get('fields')) > 0)
@@ -53,7 +54,30 @@ class TemplateAttribute extends \PCT\CustomElements\Core\TemplateAttribute
 			       $arrFields[$field] = $_this;
 		        }
 			    $objRowTemplate->set('fields',$arrFields);
-			    $objRowTemplate->set('field',$arrFields);
+			    
+			    $arrProcessed[] = $field;
+			}
+			
+			// precess the field array
+			if(count($objRowTemplate->get('field')) > 0)
+			{
+				$arrFields = $objRowTemplate->get('field');
+		    	foreach($arrFields as $field => $objAttributeTemplate)
+		        {
+			       if(in_array($field, $arrProcessed))
+			       {
+				       continue;
+			       }
+			       
+			       $_this = new self();
+				   foreach($objAttributeTemplate as $key => $val)
+				   {
+					   $_this->{$key} = $val;
+				   }
+			       $arrFields[$field] = $_this;
+		        }
+		        
+		        $objRowTemplate->set('field',array_merge($arrFields, $objRowTemplate->get('fields')) );
 			}
 			    
 	        $arrReturn[$i] = $objRowTemplate;
