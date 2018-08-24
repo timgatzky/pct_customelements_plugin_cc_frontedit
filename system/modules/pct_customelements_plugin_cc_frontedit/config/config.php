79401cc866f5354ccab286d876808c6da6806673
Tim Gatzky <info@tim-gatzky.de>
2018-08-24 08:06:46 +0200

    update to version 1.4.2

--- system/modules/pct_customelements_plugin_cc_frontedit/config/config.php
+++ system/modules/pct_customelements_plugin_cc_frontedit/config/config.php
@@ -17,7 +17,7 @@
  * Constants
  */ 
 define('PCT_CUSTOMELEMENTS_PLUGIN_CC_FRONTEDIT_PATH','system/modules/pct_customelements_plugin_cc_frontedit');
-define('PCT_CUSTOMELEMENTS_PLUGIN_CC_FRONTEDIT_VERSION','1.4.3');
+define('PCT_CUSTOMELEMENTS_PLUGIN_CC_FRONTEDIT_VERSION','1.4.2');
 
 /**
  * Globals
